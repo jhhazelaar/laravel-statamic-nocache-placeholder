d46cf1e9f1437ea712ee0b6f61cd8d6ec0c595ff
Jan Henk Hazelaar <janhenk@concept7.nl>
2023-04-03 21:35:09 +0200

    Add  middleware to dashboard route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'statamic.web'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
